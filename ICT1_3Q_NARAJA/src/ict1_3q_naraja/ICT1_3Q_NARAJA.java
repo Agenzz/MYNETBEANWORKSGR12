c45fab397745bb3715860ae627bf65b84a80caf7
Agenzz <160224915+Agenzz@users.noreply.github.com>
2025-01-13 16:40:18 +0800

    Add files via upload

---
+++ ICT1_3Q_NARAJA/src/ict1_3q_naraja/ICT1_3Q_NARAJA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ict1_3q_naraja;
+
+/**
+ *
+ * @author user
+ */
+public class ICT1_3Q_NARAJA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
